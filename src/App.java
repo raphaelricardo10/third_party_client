1e03e85380c85c08f2b85dbcf4a9525f263a92cd
Raphael <raphaelricardo10@gmail.com>
2022-11-01 00:14:12 -0300

    Feat: implemented a simple client for temperature conversion

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import com.learnwebservices.services.tempconverter.TempConverterEndpointServiceStub;
+import com.learnwebservices.services.tempconverter.TempConverterEndpointServiceStub.CelsiusToFahrenheitRequest;
+import com.learnwebservices.services.tempconverter.TempConverterEndpointServiceStub.CelsiusToFahrenheitRequestE;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        TempConverterEndpointServiceStub stub = new TempConverterEndpointServiceStub("https://apps.learnwebservices.com/services/tempconverter?wsdl");
+        CelsiusToFahrenheitRequest request = new CelsiusToFahrenheitRequest();
+        request.setTemperatureInCelsius(32.0);
+
+        CelsiusToFahrenheitRequestE requestE = new CelsiusToFahrenheitRequestE();
+        requestE.setCelsiusToFahrenheitRequest(request);
+
+        stub.celsiusToFahrenheit(requestE).getCelsiusToFahrenheitResponse().getTemperatureInFahrenheit();
+
+        double temperatureInFahrenheit = stub.celsiusToFahrenheit(requestE).getCelsiusToFahrenheitResponse().getTemperatureInFahrenheit();
+
+        System.out.println(temperatureInFahrenheit);
     }
 }
